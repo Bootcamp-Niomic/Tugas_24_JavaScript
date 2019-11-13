1d2755c013388d2bb84b8b20ace496f17d0caed8
Havid <great_havid@yahoo.co.id>
2019-11-13 11:46:44 +0700

    Tugas 24 JS

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+// ====== Tugas 24 JavaScript =====
+
+var data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("Sebelumnya : ", data.join())
+
+data2 = data.sort()
+console.log("Ascending : ", data2.join())
+data3 = data.reverse()
+console.log("Descending : ", data3.join());
